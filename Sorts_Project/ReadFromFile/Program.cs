9b311d54d5559fb02b5144cbfdaaeafc716234b5
Darius Kulevicius <dariuux@gmail.com>
2020-02-13 22:46:10 +0200

    Sorts_Project added

---
+++ Sorts_Project/ReadFromFile/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ReadFromFile
+{
+    class Node
+    {
+        int key;
+        Node left;
+        Node right;
+
+        public Node()
+        {
+            left = null;
+            right = null;
+            key = -1;
+        }
+
+        Node newNode()
+        {
+            Node temp = new Node();
+            temp.key = key;
+            temp.right = temp.left = null;
+            return temp;
+        }
+
+
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
