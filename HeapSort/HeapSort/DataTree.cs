6991a04baff15ce534714cb9a70bb6503d61b9c8
Darius Kulevicius <dariuux@gmail.com>
2020-02-14 10:44:49 +0200

    .

---
+++ HeapSort/HeapSort/DataTree.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeapSort
+{
+    class Node
+    {
+       public float key;
+       public Node left, right;
+
+        public Node(float item)
+        {
+            key = item;
+            left = right = null;
+        }
+
+        public Node()
+        {
+            
+        }
+
+
+
+    }
+
+
+    class DataTree
+    {
+        public Node root;
+        int count;
+
+        public DataTree(float key)
+        {
+            root = new Node(key);
+            count = 1;
+        }
+
+        public DataTree()
+        {
+            root = null;
+            count = 0;
+        }
+
+        public Node insertNode(Node head, float data)
+        {
+            if (head == null)
+            {
+                head = new Node();
+                head.key = data;
+                return head;
+            }
+
+            if (head.key < data)
+            {
+                head.right = insertNode(head.right, data);
+            }
+            else
+            {
+                head.left = insertNode(head.left, data);
+            }
+            return head;
+
+        }
+
+        public void insert(float data)
+        {
+            count++;
+            root = insertNode(root, data);
+        }
+
+        public void printTree(Node node)
+        {
+            if (node == null)
+                return;
+            else
+            {
+                Console.WriteLine(node.key);
+                printTree(node.left);
+                printTree(node.right);
+            }
+
+
+        }
+
+        public int getCount()
+        {
+            return count;
+        }
+
+
+
+        public void print()
+        {
+            printTree(root);
+        }
+
+
+    }
+
+    class HeapSort
+    {
+        
+    }
+
+      
+
+}
---
+++ HeapSort/HeapSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HeapSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataTree tree = new DataTree();
+
+            Random r = new Random();
+            float a;
+            for (int i = 0; i < 10; i++)
+            {
+                a = (float)r.NextDouble();
+                tree.insert(a);
+            }
+
+            tree.print();
+        }
+    }
+}
